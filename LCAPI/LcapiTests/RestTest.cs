ff26b481d5322d69042265032a866f9848cf3265
J. Zebedee <zebedee@code.gripe>
2016-02-20 19:48:37 -0600

    Add test stub

---
+++ LCAPI/LcapiTests/RestTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace LcapiTests
+{
+    public class RestTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}

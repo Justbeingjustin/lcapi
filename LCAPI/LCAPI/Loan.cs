1f12a7ec2e544afb4bfe265d6921007600dcbb65
J. Zebedee <zebedee@code.gripe>
2016-02-21 01:54:40 -0600

    Add XML documentation to GetListing

--- LCAPI/LCAPI/Loan.cs
+++ LCAPI/LCAPI/Loan.cs
@@ -16,8 +16,14 @@ namespace LendingClub
         protected string ListingUrl => $"{Url}/listing";
 
         /// <summary>
-        /// 
+        /// Details of the loans currently listed on the Lending Club platform.
+        /// The list contains the same loans that an investor would see on the browse loans page on the Lending Club website.
         /// </summary>
+        /// <remarks>
+        /// The same restrictions that Lending Club applies on the browse loans page also apply to the browse loans API.
+        /// The API currently returns either the entire list of loans listed on the platform or only the loans listed in the recent listing period.
+        /// Loans are listed on the Lending Club platform at 6AM, 10AM, 2PM, and 6PM every day.
+        /// </remarks>
         /// <param name="showAll">
         /// <para>
         /// An optional parameter that defines the contents of the result.<br/>
@@ -34,8 +40,14 @@ namespace LendingClub
         }
 
         /// <summary>
-        /// 
+        /// Details of the loans currently listed on the Lending Club platform.
+        /// The list contains the same loans that an investor would see on the browse loans page on the Lending Club website.
         /// </summary>
+        /// <remarks>
+        /// The same restrictions that Lending Club applies on the browse loans page also apply to the browse loans API.
+        /// The API currently returns either the entire list of loans listed on the platform or only the loans listed in the recent listing period.
+        /// Loans are listed on the Lending Club platform at 6AM, 10AM, 2PM, and 6PM every day.
+        /// </remarks>
         /// <param name="showAll">
         /// <para>
         /// An optional parameter that defines the contents of the result.<br/>

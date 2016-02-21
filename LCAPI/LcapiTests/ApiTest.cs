89e294beaac0703c34cd967e784eef622807330d
J. Zebedee <zebedee@code.gripe>
2016-02-21 01:19:21 -0600

    Add test stubs

---
+++ LCAPI/LcapiTests/ApiTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LendingClub;
+using Xunit;
+
+namespace LcapiTests
+{
+    public class ApiTest
+    {
+        private KeyValuePair<string, string> GetTestCredentials()
+        {
+            var lines = File.ReadLines(@"C:\Dropbox\Data\LocalRepos\lcapi\LCAPI\testCredentials.txt").ToList();
+            return new KeyValuePair<string, string>(lines.First(), lines.Last());
+        }
+
+        private Api CreateApiObject()
+        {
+            var cred = GetTestCredentials();
+            var customer = cred.Key;
+            var apiKey = cred.Value;
+
+            return new Api(customer, apiKey);
+        }
+
+        [Fact]
+        public void SummaryTest()
+        {
+            var api = CreateApiObject();
+            var task = api.GetSummaryAsync();
+            var result = task.Result;
+
+            Assert.NotNull(result);
+        }
+    }
+}

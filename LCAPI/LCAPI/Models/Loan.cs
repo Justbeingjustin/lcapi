0406fe35b9509ca0d9eb932262d6c075c2910cff
J. Zebedee <zebedee@code.gripe>
2016-02-21 02:18:50 -0600

    Break out Loan from LoanListing

---
+++ LCAPI/LCAPI/Models/Loan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LCAPI.JSON;
+
+namespace LendingClub.Models
+{
+    /// <summary>
+    /// Details of a loan currently listed on the Lending Club platform.
+    /// </summary>
+    public class Loan
+    {
+        public int Id { get; set; }
+
+        public int MemberId { get; set; }
+
+        public int Term { get; set; }
+
+        [DeserializeAs("intRate")]
+        public decimal InterestRate { get; set; }
+    }
+}
--- LCAPI/LCAPI/Models/LoanListing.cs
+++ LCAPI/LCAPI/Models/LoanListing.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LCAPI.JSON;
 
 namespace LendingClub.Models
 {
+    /// <summary>
+    /// Details of the loans currently listed on the Lending Club platform.
+    /// The list contains the same loans that an investor would see on the browse loans page on the Lending Club website.
+    /// </summary>
     public class LoanListing
     {
+        public DateTime AsOfDate { get; set; }
+
+        public List<Loan> Loans { get; set; }
     }
 }

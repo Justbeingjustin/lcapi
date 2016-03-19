b1bb13ff8781a7e0723db12b6bb51048a89c873f
J. Zebedee <zebedee@code.gripe>
2016-03-19 18:22:24 -0500

    Add AccountTransferFunds model

---
+++ LCAPI/LCAPI/Models/AccountTransferFunds.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendingClub.Models
+{
+    public class AccountTransferFunds
+    {
+        /// <summary>
+        /// Investor ID
+        /// </summary>
+        public int InvestorId { get; set; }
+
+        /// <summary>
+        /// Amount of funds
+        /// </summary>
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Frequency of funds transfer
+        /// </summary>
+        public TransferFrequency TransferFrequency { get; set; }
+
+        /// <summary>
+        /// Recurring transfer start date or transfer date for one-time transfers
+        /// </summary>
+        public DateTime? StartDate { get; set; }
+
+        /// <summary>
+        /// Recurring transfer end date or null in case of one-time transfers
+        /// </summary>
+        public DateTime? EndDate { get; set; }
+
+        /// <summary>
+        /// Estimated start date of funds transfer
+        /// </summary>
+        public DateTime EstimatedFundsTransferDate { get; set; }
+    }
+}

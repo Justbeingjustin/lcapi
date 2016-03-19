405b6c301e704b19bdad6f702ecef206d15e5551
J. Zebedee <zebedee@code.gripe>
2016-03-19 14:58:10 -0500

    Add AccountAvailableCash enum

---
+++ LCAPI/LCAPI/Models/AccountAvailableCash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendingClub.Models
+{
+    /// <summary>
+    /// Provides the most up to date value of the cash available in the investor's account.
+    /// </summary>
+    public class AccountAvailableCash
+    {
+        /// <summary>
+        /// Investor ID
+        /// </summary>
+        public int InvestorId { get; set; }
+
+        /// <summary>
+        /// Available cash amount
+        /// </summary>
+        public decimal AvailableCash { get; set; }
+    }
+}

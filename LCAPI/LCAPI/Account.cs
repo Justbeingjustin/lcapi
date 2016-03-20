50b87d8ee209682f41c1a9328dd7bf7a2cc285ff
J. Zebedee <zebedee@code.gripe>
2016-03-19 23:11:42 -0500

    Add and implement AddFunds to Account
    - Add WithdrawFunds stub

--- LCAPI/LCAPI/Account.cs
+++ LCAPI/LCAPI/Account.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using LendingClub.Models;
 
 namespace LendingClub
@@ -40,16 +41,83 @@ namespace LendingClub
 
         protected string AddFundsUrl => $"{Url}/funds/add";
 
-        public Task<AccountAvailableCash> AddFundsAsync(decimal amount)
+        private Task<AccountTransferFunds> AddFundsAsync<T>(T request)
         {
-            return Client.GetAsync<AccountAvailableCash>(AddFundsUrl);
+            return Client.PostAsync<AccountTransferFunds, T>(AddFundsUrl, request);
+        }
+
+        public Task<AccountTransferFunds> AddFundsNowAsync(decimal amount)
+        {
+            return AddFundsAsync(new
+            {
+                transferFrequency = "LOAD_NOW",
+                amount
+            });
+        }
+
+        public Task<AccountTransferFunds> AddFundsScheduledAsync(decimal amount, DateTime startDate)
+        {
+            return AddFundsAsync(new
+            {
+                transferFrequency = "LOAD_ONCE",
+                amount,
+                startDate = startDate.ToUniversalTime()
+            });
+        }
+
+        public Task<AccountTransferFunds> AddFundsRecurringWeeklyAsync(decimal amount, DateTime startDate,
+            DateTime endDate)
+        {
+            return AddFundsAsync(new
+            {
+                transferFrequency = "LOAD_WEEKLY",
+                amount,
+                startDate = startDate.ToUniversalTime(),
+                endDate = endDate.ToUniversalTime()
+            });
+        }
+
+        public Task<AccountTransferFunds> AddFundsRecurringBiWeeklyAsync(decimal amount, DateTime startDate,
+            DateTime endDate)
+        {
+            return AddFundsAsync(new
+            {
+                transferFrequency = "LOAD_BIWEEKLY",
+                amount,
+                startDate = startDate.ToUniversalTime(),
+                endDate = endDate.ToUniversalTime()
+            });
+        }
+
+        public Task<AccountTransferFunds> AddFundsRecurringBiMonthlyAsync(decimal amount, DateTime startDate,
+            DateTime endDate)
+        {
+            return AddFundsAsync(new
+            {
+                transferFrequency = "LOAD_ON_DAY_1_AND_16",
+                amount,
+                startDate = startDate.ToUniversalTime(),
+                endDate = endDate.ToUniversalTime()
+            });
+        }
+
+        public Task<AccountTransferFunds> AddFundsRecurringMonthlyAsync(decimal amount, DateTime startDate,
+            DateTime endDate)
+        {
+            return AddFundsAsync(new
+            {
+                transferFrequency = "LOAD_MONTHLY",
+                amount,
+                startDate = startDate.ToUniversalTime(),
+                endDate = endDate.ToUniversalTime()
+            });
         }
 
         protected string WithdrawFundsUrl => $"{Url}/funds/withdraw";
 
-        public Task<AccountAvailableCash> WithdrawFundsAsync()
+        public Task<AccountTransferFunds> WithdrawFundsAsync()
         {
-            return Client.GetAsync<AccountAvailableCash>(WithdrawFundsUrl);
+            return Client.GetAsync<AccountTransferFunds>(WithdrawFundsUrl);
         }
 
         #endregion

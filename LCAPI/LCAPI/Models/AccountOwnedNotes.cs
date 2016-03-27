4b47c81ef99640155335a656f5455d00e86cf34e
J. Zebedee <zebedee@code.gripe>
2016-03-27 07:15:43 -0500

    Add AccountOwnedNotes model

---
+++ LCAPI/LCAPI/Models/AccountOwnedNotes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using LCAPI.JSON;
+
+namespace LendingClub.Models
+{
+    /// <summary>
+    /// Pending fund transfers for the investor's account
+    /// </summary>
+    public class AccountOwnedNotes : IReadOnlyList<Note>
+    {
+        [DeserializeAs("myNotes")]
+        private List<Note> MyNotes { get; set; }
+
+        public IEnumerator<Note> GetEnumerator() => MyNotes.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)MyNotes).GetEnumerator();
+
+        public int Count => MyNotes.Count;
+
+        public Note this[int index] => MyNotes[index];
+    }
+}

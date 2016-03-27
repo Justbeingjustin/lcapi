281c1f870e16fb6ce9749c46e15ee5ee1435cbbf
J. Zebedee <zebedee@code.gripe>
2016-03-27 07:43:36 -0500

    Add LoanStatus enum

---
+++ LCAPI/LCAPI/Models/LoanStatus.cs
@@ -0,0 +1,17 @@
+﻿// ReSharper disable InconsistentNaming
+namespace LendingClub.Models
+{
+    public enum LoanStatus
+    {
+        None = 0,
+        Issued,
+        InReview,
+        FullyPaid,
+        Current,
+        InGracePeriod,
+        Late_16_30_Days,
+        Late_31_120_Days,
+        Default,
+        ChargedOff
+    }
+}
